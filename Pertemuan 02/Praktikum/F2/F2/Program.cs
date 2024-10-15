c5da1d292442892f5781df549ea558022a0b6328
mrestuab <mrestuab54@gmail.com>
2024-10-15 16:09:57 +0700

    praktikum 2

---
+++ Pertemuan 02/Praktikum/F2/F2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukan angka pertama :");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.Write("masukan angka pertama :");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("angka pertama = " + input);
+            Console.WriteLine("angka kedua = " + input2);
+            Console.WriteLine(input + "+" + input2 + "=" + (input + input2));
+            Console.WriteLine(input + "-" + input2 + "=" + (input - input2));
+            Console.WriteLine(input + "*" + input2 + "=" + (input * input2));
+            Console.WriteLine(input + "/" + input2 + "=" + (input / input2));
+        }
+    }
+}
